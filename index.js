31ff9a86fd0fd1515c1c4b73c8ed63c0297a243b
nicole0320 <difan0320@126.com>
2017-07-31 19:10:36 +0800

    把所有let声明的变量都改为var声明

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-let channelsList = [];
-let currentChannels = [];
-let currentChannelsId = 1;
-let currentSong = {};
-let playing = true;
-let clock = 1;
-let time = 0;
-let duration = 0;
-let lyric = [];
-let lyricClock = -2;
+var channelsList = [];
+var currentChannels = [];
+var currentChannelsId = 1;
+var currentSong = {};
+var playing = true;
+var clock = 1;
+var time = 0;
+var duration = 0;
+var lyric = [];
+var lyricClock = -2;
 
 $(window).ready(function(){
     $('audio').attr('autoplay', 'true');
-    let lyricBoxHeight = $('.fm-container').innerHeight()-$('#main').outerHeight()-$('.topbar').outerHeight()-60;
+    var lyricBoxHeight = $('.fm-container').innerHeight()-$('#main').outerHeight()-$('.topbar').outerHeight()-60;
     $('.lyric').css('height',lyricBoxHeight+'px');
 
     // 获取播放频道
     // $.get('http://api.jirengu.com/fm/getChannels.php')
     // .done(function(channelInfo){
     //     channelsList = JSON.parse(channelInfo).channels;
-    //     let obj = {channel_id: '', name: ''};
+    //     var obj = {channel_id: '', name: ''};
     //     channelsList.push(obj);
     //     channelsList.unshift(obj);
     //     currentChannels = channelsList.slice(0, 3);
@@ -28,7 +28,7 @@ $(window).ready(function(){
 
     //为解决github page的https协议，把频道列表写死
     channelsList = [{"channel_id":"","name":""},{"name":"漫步春天","channel_id":"public_tuijian_spring"},{"name":"秋日私语","channel_id":"public_tuijian_autumn"},{"name":"温暖冬日","channel_id":"public_tuijian_winter"},{"name":"热歌","channel_id":"public_tuijian_rege"},{"name":"KTV金曲","channel_id":"public_tuijian_ktv"},{"name":"Billboard","channel_id":"public_tuijian_billboard"},{"name":"成名曲","channel_id":"public_tuijian_chengmingqu"},{"name":"网络歌曲","channel_id":"public_tuijian_wangluo"},{"name":"开车","channel_id":"public_tuijian_kaiche"},{"name":"影视","channel_id":"public_tuijian_yingshi"},{"name":"随便听听","channel_id":"public_tuijian_suibiantingting"},{"name":"经典老歌","channel_id":"public_shiguang_jingdianlaoge"},{"name":"70后","channel_id":"public_shiguang_70hou"},{"name":"80后","channel_id":"public_shiguang_80hou"},{"name":"90后","channel_id":"public_shiguang_90hou"},{"name":"火爆新歌","channel_id":"public_shiguang_xinge"},{"name":"儿歌","channel_id":"public_shiguang_erge"},{"name":"旅行","channel_id":"public_shiguang_lvxing"},{"name":"夜店","channel_id":"public_shiguang_yedian"},{"name":"流行","channel_id":"public_fengge_liuxing"},{"name":"摇滚","channel_id":"public_fengge_yaogun"},{"name":"民谣","channel_id":"public_fengge_minyao"},{"name":"轻音乐","channel_id":"public_fengge_qingyinyue"},{"name":"小清新","channel_id":"public_fengge_xiaoqingxin"},{"name":"中国风","channel_id":"public_fengge_zhongguofeng"},{"name":"DJ舞曲","channel_id":"public_fengge_dj"},{"name":"电影","channel_id":"public_fengge_dianyingyuansheng"},{"name":"轻松假日","channel_id":"public_xinqing_qingsongjiari"},{"name":"欢快旋律","channel_id":"public_xinqing_huankuai"},{"name":"甜蜜感受","channel_id":"public_xinqing_tianmi"},{"name":"寂寞","channel_id":"public_xinqing_jimo"},{"name":"单身情歌","channel_id":"public_xinqing_qingge"},{"name":"舒缓节奏","channel_id":"public_xinqing_shuhuan"},{"name":"慵懒午后","channel_id":"public_xinqing_yonglanwuhou"},{"name":"伤感","channel_id":"public_xinqing_shanggan"},{"name":"华语","channel_id":"public_yuzhong_huayu"},{"name":"欧美","channel_id":"public_yuzhong_oumei"},{"name":"日语","channel_id":"public_yuzhong_riyu"},{"name":"韩语","channel_id":"public_yuzhong_hanyu"},{"name":"粤语","channel_id":"public_yuzhong_yueyu"},{"channel_id":"","name":""}]
-    let obj = {channel_id: '', name: ''};
+    var obj = {channel_id: '', name: ''};
     currentChannels = channelsList.slice(0, 3);
     updateChannels();
     loadSong();
@@ -82,14 +82,14 @@ $('.play-or-pause').on('click', function(){
 $('.open-channels').on('click', function(){
     $channels = $('#channels');
     $.each(channelsList, function(key, value){
-        let tempHTML = '<li data="'+ value.channel_id +'>'+value.name+'</li>';
+        var tempHTML = '<li data="'+ value.channel_id +'>'+value.name+'</li>';
         $channels.append(tempHTML);
     })
 })
 
 //加载播放信息
 $('audio').on('canplay', function(){
-    let $audio = $('audio')[0];
+    var $audio = $('audio')[0];
     duration = $audio.duration;
     time = 0;
     clock = setInterval(function(){
@@ -98,7 +98,7 @@ $('audio').on('canplay', function(){
             return;
         }
         time = $audio.currentTime;
-        let timeRate = Math.floor(time/duration*1000)/10 + '%';
+        var timeRate = Math.floor(time/duration*1000)/10 + '%';
         $('.played').css('width', timeRate);
         leftTime(duration - time);
     }, 1000);
@@ -109,13 +109,13 @@ $('audio').on('ended', function(){
 })
 
 $('.time-line>.line').on('click', function(e){
-    let position = e.pageX - $(this).offset().left;
+    var position = e.pageX - $(this).offset().left;
     time = position/$(this).innerWidth()*duration;
     $('audio')[0].currentTime = time;
 })
 
 function updateChannels(){
-    for(let i=0; i<3; i++){
+    for(var i=0; i<3; i++){
         $('.channels').children().eq(i).html(currentChannels[i].name);
     }
 }
@@ -133,8 +133,8 @@ function loadSong(){
                 loadSong();
             }
             else{
-                let indexOfAt = currentSong.picture.indexOf('@');
-                let pictureURL = currentSong.picture.substring(0,indexOfAt);
+                var indexOfAt = currentSong.picture.indexOf('@');
+                var pictureURL = currentSong.picture.substring(0,indexOfAt);
                 lyric = [];
                 $('img').attr('src', pictureURL);
                 $('.artist').html(currentSong.artist);
@@ -149,7 +149,7 @@ function loadLrc(songID){
     $.post('https://jirenguapi.applinzi.com/fm/getLyric.php',{sid: songID})
     .done(function(res){
         clearInterval(lyricClock);
-        let curLrcNum = 0;
+        var curLrcNum = 0;
         decodeLyric(JSON.parse(res).lyric);
         $('.lyric').html(lyric);
         renderLyric();
@@ -163,14 +163,14 @@ function loadLrc(songID){
 }
 
 function decodeLyric(lrcString){
-    let lrcArr = lrcString.split('\n');
+    var lrcArr = lrcString.split('\n');
     lrcArr.forEach(function(element) {
-        let lrcObj = {};
-        let lrcTime = element.match(/\[(?:\d+:)\d+.\d+]/g);
-        let lrcContent = element.match(/\][^\[].*/g)
+        var lrcObj = {};
+        var lrcTime = element.match(/\[(?:\d+:)\d+.\d+]/g);
+        var lrcContent = element.match(/\][^\[].*/g)
         if(lrcTime !== null){
             lrcTime.forEach(function(ele){
-                let min = 0, sec = 0, secTime = 0;
+                var min = 0, sec = 0, secTime = 0;
                 min = ele.match(/\[\d+:/)[0].slice(1,3);
                 sec = ele.match(/\:\d+.\d+/)[0].slice(1,6);
                 secTime = parseInt(min)*60 + parseFloat(sec);
@@ -191,21 +191,21 @@ function decodeLyric(lrcString){
 
 function renderLyric(){
     $('.lyric').empty();
-    let end = {
+    var end = {
         content: "~ end ~",
         time: duration-0.1
     }
     lyric.push(end);
     $.each(lyric, function(key,value){
-        let html = '<li>'+ value.content +'</li>';
+        var html = '<li>'+ value.content +'</li>';
         $('.lyric').append(html)
     });
 }
 
 function leftTime(num){
     num = Math.floor(num);
-    let sec = num%60;
-    let min = Math.floor(num/60);
+    var sec = num%60;
+    var min = Math.floor(num/60);
     if(min<10){
         $('.minute').html('0'+min);
     }
@@ -245,13 +245,13 @@ function renderScrollLyric(curLrcNum){
     $lyric.children('li').removeClass('current-line');
     $lyric.children('li').eq(curLrcNum).addClass('current-line');
     //单行歌词的高度
-    let lineHeight = $lyric.children('li').eq(0).outerHeight()+5;
+    var lineHeight = $lyric.children('li').eq(0).outerHeight()+5;
     //第一行歌词顶端到当前歌词的距离
-    let curOffenset = lineHeight*(curLrcNum+1) + $('.current-line').outerHeight(true)/2;
+    var curOffenset = lineHeight*(curLrcNum+1) + $('.current-line').outerHeight(true)/2;
     //整篇歌词的实际高度
-    let lrcLength = $('.current-line').outerHeight(true)*lyric.length;
+    var lrcLength = $('.current-line').outerHeight(true)*lyric.length;
     //显示歌词的可见窗口的高度
-    let height = $lyric.height();
+    var height = $lyric.height();
 
     if(curOffenset>(height/2) && lrcLength-curOffenset > height/2){
         $lyric.scrollTop(curOffenset - height/2);
