887c2ac3d426543398f0b7cefd5ae0d699d66f70
nicole0320 <difan0320@126.com>
2017-08-01 18:30:04 +0800

    切换到“我喜欢的”列表时播放收藏进“喜欢”列表的歌曲

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var channelsList = [];
 var currentChannels = [];
-var currentChannelsId = 1;
+var currentChannelsId = 2;
 var currentSong = {};
 var playing = true;
 var clock = 1;
@@ -22,18 +22,19 @@ $(window).ready(function(){
     // $.get('http://api.jirengu.com/fm/getChannels.php')
     // .done(function(channelInfo){
     //     channelsList = JSON.parse(channelInfo).channels;
+    //     channelsList.unshift({"channel_id":"favorite","name":"我喜欢的"})
     //     var obj = {channel_id: '', name: ''};
     //     channelsList.push(obj);
     //     channelsList.unshift(obj);
-    //     currentChannels = channelsList.slice(0, 3);
+    //     currentChannels = channelsList.slice(1, 4);
     //     updateChannels();
     //     loadSong();
     // });
 
     //为解决github page的https协议，把频道列表写死
-    channelsList = [{"channel_id":"","name":""},{"name":"漫步春天","channel_id":"public_tuijian_spring"},{"name":"秋日私语","channel_id":"public_tuijian_autumn"},{"name":"温暖冬日","channel_id":"public_tuijian_winter"},{"name":"热歌","channel_id":"public_tuijian_rege"},{"name":"KTV金曲","channel_id":"public_tuijian_ktv"},{"name":"Billboard","channel_id":"public_tuijian_billboard"},{"name":"成名曲","channel_id":"public_tuijian_chengmingqu"},{"name":"网络歌曲","channel_id":"public_tuijian_wangluo"},{"name":"开车","channel_id":"public_tuijian_kaiche"},{"name":"影视","channel_id":"public_tuijian_yingshi"},{"name":"随便听听","channel_id":"public_tuijian_suibiantingting"},{"name":"经典老歌","channel_id":"public_shiguang_jingdianlaoge"},{"name":"70后","channel_id":"public_shiguang_70hou"},{"name":"80后","channel_id":"public_shiguang_80hou"},{"name":"90后","channel_id":"public_shiguang_90hou"},{"name":"火爆新歌","channel_id":"public_shiguang_xinge"},{"name":"儿歌","channel_id":"public_shiguang_erge"},{"name":"旅行","channel_id":"public_shiguang_lvxing"},{"name":"夜店","channel_id":"public_shiguang_yedian"},{"name":"流行","channel_id":"public_fengge_liuxing"},{"name":"摇滚","channel_id":"public_fengge_yaogun"},{"name":"民谣","channel_id":"public_fengge_minyao"},{"name":"轻音乐","channel_id":"public_fengge_qingyinyue"},{"name":"小清新","channel_id":"public_fengge_xiaoqingxin"},{"name":"中国风","channel_id":"public_fengge_zhongguofeng"},{"name":"DJ舞曲","channel_id":"public_fengge_dj"},{"name":"电影","channel_id":"public_fengge_dianyingyuansheng"},{"name":"轻松假日","channel_id":"public_xinqing_qingsongjiari"},{"name":"欢快旋律","channel_id":"public_xinqing_huankuai"},{"name":"甜蜜感受","channel_id":"public_xinqing_tianmi"},{"name":"寂寞","channel_id":"public_xinqing_jimo"},{"name":"单身情歌","channel_id":"public_xinqing_qingge"},{"name":"舒缓节奏","channel_id":"public_xinqing_shuhuan"},{"name":"慵懒午后","channel_id":"public_xinqing_yonglanwuhou"},{"name":"伤感","channel_id":"public_xinqing_shanggan"},{"name":"华语","channel_id":"public_yuzhong_huayu"},{"name":"欧美","channel_id":"public_yuzhong_oumei"},{"name":"日语","channel_id":"public_yuzhong_riyu"},{"name":"韩语","channel_id":"public_yuzhong_hanyu"},{"name":"粤语","channel_id":"public_yuzhong_yueyu"},{"channel_id":"","name":""}]
+    channelsList = [{"channel_id":"","name":""},{"channel_id":"favorite","name":"我喜欢的"},{"name":"漫步春天","channel_id":"public_tuijian_spring"},{"name":"秋日私语","channel_id":"public_tuijian_autumn"},{"name":"温暖冬日","channel_id":"public_tuijian_winter"},{"name":"热歌","channel_id":"public_tuijian_rege"},{"name":"KTV金曲","channel_id":"public_tuijian_ktv"},{"name":"Billboard","channel_id":"public_tuijian_billboard"},{"name":"成名曲","channel_id":"public_tuijian_chengmingqu"},{"name":"网络歌曲","channel_id":"public_tuijian_wangluo"},{"name":"开车","channel_id":"public_tuijian_kaiche"},{"name":"影视","channel_id":"public_tuijian_yingshi"},{"name":"随便听听","channel_id":"public_tuijian_suibiantingting"},{"name":"经典老歌","channel_id":"public_shiguang_jingdianlaoge"},{"name":"70后","channel_id":"public_shiguang_70hou"},{"name":"80后","channel_id":"public_shiguang_80hou"},{"name":"90后","channel_id":"public_shiguang_90hou"},{"name":"火爆新歌","channel_id":"public_shiguang_xinge"},{"name":"儿歌","channel_id":"public_shiguang_erge"},{"name":"旅行","channel_id":"public_shiguang_lvxing"},{"name":"夜店","channel_id":"public_shiguang_yedian"},{"name":"流行","channel_id":"public_fengge_liuxing"},{"name":"摇滚","channel_id":"public_fengge_yaogun"},{"name":"民谣","channel_id":"public_fengge_minyao"},{"name":"轻音乐","channel_id":"public_fengge_qingyinyue"},{"name":"小清新","channel_id":"public_fengge_xiaoqingxin"},{"name":"中国风","channel_id":"public_fengge_zhongguofeng"},{"name":"DJ舞曲","channel_id":"public_fengge_dj"},{"name":"电影","channel_id":"public_fengge_dianyingyuansheng"},{"name":"轻松假日","channel_id":"public_xinqing_qingsongjiari"},{"name":"欢快旋律","channel_id":"public_xinqing_huankuai"},{"name":"甜蜜感受","channel_id":"public_xinqing_tianmi"},{"name":"寂寞","channel_id":"public_xinqing_jimo"},{"name":"单身情歌","channel_id":"public_xinqing_qingge"},{"name":"舒缓节奏","channel_id":"public_xinqing_shuhuan"},{"name":"慵懒午后","channel_id":"public_xinqing_yonglanwuhou"},{"name":"伤感","channel_id":"public_xinqing_shanggan"},{"name":"华语","channel_id":"public_yuzhong_huayu"},{"name":"欧美","channel_id":"public_yuzhong_oumei"},{"name":"日语","channel_id":"public_yuzhong_riyu"},{"name":"韩语","channel_id":"public_yuzhong_hanyu"},{"name":"粤语","channel_id":"public_yuzhong_yueyu"},{"channel_id":"","name":""}]
     var obj = {channel_id: '', name: ''};
-    currentChannels = channelsList.slice(0, 3);
+    currentChannels = channelsList.slice(1, 4);
     updateChannels();
     loadSong();
     loadPlaylist();
@@ -66,6 +67,14 @@ $('.prev').on('click', function(){
     currentChannelsId--;
 
     currentChannels = channelsList.slice(currentChannelsId-1, currentChannelsId+2);
+    if(currentChannels[1].channel_id === 'favorite'){
+        if(likePlaylist.playlist.length === 0){
+            alert('“我喜欢的”音乐播放列表还是空的哦，先收藏几首喜欢的音乐吧~');
+            currentChannelsId++;
+            currentChannels = channelsList.slice(currentChannelsId-1, currentChannelsId+2);
+            return;
+        }
+    }
     updateChannels();
     loadSong();
 })
@@ -172,25 +181,43 @@ function loadSong(){
     $('.minute').html('00');
     $('.like').removeClass('chosen');
     //当前列表不是“我喜欢的音乐”则从API获取当前频道的随机歌曲，否则从“喜欢”列表加载
-    $.get('https://jirenguapi.applinzi.com/fm/getSong.php',{channel: currentChannels[1].channel_id})
-        .done(function(song){
-            currentSong = JSON.parse(song).song[0];
-            if(currentSong.url === null){
-                console.log('这首歌是空的，再来一首')
-                loadSong();
-            }
-            else{
-                var indexOfAt = currentSong.picture.indexOf('@');
-                var pictureURL = currentSong.picture.substring(0,indexOfAt);
-                lyric = [];
-                $('img').attr('src', pictureURL);
-                $('.artist').html(currentSong.artist);
-                $('.title').html(currentSong.title);
-                $('audio').attr('src', currentSong.url);
-                loadLrc(currentSong.sid);
-                isLiked(currentSong);
-            }
-        });
+    if(currentChannels[1].channel_id === 'favorite'){
+        if(likePlaylist.playing === -1){
+            currentSong = likePlaylist.playlist[0];
+            likePlaylist.playing = 0;
+        }
+        else{
+            likePlaylist.playing = (likePlaylist.playing + 1 ) % likePlaylist.playlist.length;
+            currentSong = likePlaylist.playlist[likePlaylist.playing];
+        }
+        
+        loadDetailsOfSong();
+    }
+    else{
+        $.get('https://jirenguapi.applinzi.com/fm/getSong.php',{channel: currentChannels[1].channel_id})
+            .done(function(song){
+                currentSong = JSON.parse(song).song[0];
+                if(currentSong.url === null){
+                    console.log('这首歌是空的，再来一首')
+                    loadSong();
+                }
+                else{
+                    loadDetailsOfSong();
+                }
+            });
+    }
+}
+
+function loadDetailsOfSong(){
+    var indexOfAt = currentSong.picture.indexOf('@');
+    var pictureURL = currentSong.picture.substring(0,indexOfAt);
+    lyric = [];
+    $('img').attr('src', pictureURL);
+    $('.artist').html(currentSong.artist);
+    $('.title').html(currentSong.title);
+    $('audio').attr('src', currentSong.url);
+    loadLrc(currentSong.sid);
+    isLiked(currentSong);
 }
 
 function loadLrc(songID){
